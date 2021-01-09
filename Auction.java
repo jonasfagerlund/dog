8915da317db43a78f19f54de638faf48f3f48d0d
jonasafagerlund <jonas@kladkod.com>
2021-01-09 11:44:33 +0100

    Removed everything unnecessary

--- Auction.java
+++ Auction.java
@@ -5,88 +5,88 @@
 import java.util.ArrayList;
 
 public class Auction {
-  
-  private static int id = 1;
 
-  private int auctionId;
-  private Dog dog;
-  private ArrayList<Bid> bids = new ArrayList<>();
+    private static int id = 1;
 
-  public Auction(Dog d){
-    this.dog = d;
-    this.auctionId = Auction.id;
-    Auction.id++;
+    private int auctionId;
+    private Dog dog;
+    private ArrayList<Bid> bids = new ArrayList<>();
 
-    d.setAuction(this);
-  }
+    public Auction(Dog d) {
+        this.dog = d;
+        this.auctionId = Auction.id;
+        Auction.id++;
 
-  public void addBid(Bid b){
-    for(int i = 0; i < bids.size(); i++){
-      if(bids.get(i).getBidder() == b.getBidder())
-        removeBid(bids.get(i));
-      }
-    bids.add(b);
-  }
-
-  public void removeBid(Bid b){
-    bids.remove(b);
-  }
-
-  public boolean haveBid(){
-    return !bids.isEmpty();
-  }
-
-  public void printBids(){
-    for(int i = bids.size() - 1; i >= 0; i--){
-      System.out.println(bids.get(i));
+        d.setAuction(this);
     }
-  }
 
-  public void printThreeBids(){
-    int counter = bids.size();
-    if (bids.size() > 2 ){
-      counter = 3;
+    public void addBid(Bid b) {
+        for (int i = 0; i < bids.size(); i++) {
+            if (bids.get(i).getBidder() == b.getBidder())
+                removeBid(bids.get(i));
+        }
+        bids.add(b);
     }
-    for(int i = bids.size() - 1; i >= bids.size() - counter; i--){
-      System.out.println(bids.get(i));
+
+    public void removeBid(Bid b) {
+        bids.remove(b);
     }
-  }
 
-  public Bid getHighestBid(){
-    if (bids.size() > 0) {
-      return bids.get(bids.size() - 1);
+    public boolean haveBid() {
+        return !bids.isEmpty();
     }
-    return null;
-  }
 
-  public Bid getBidFromGivenUser(Owner o){
-    for(Bid b : bids){
-      if (b.getBidder() == o) {
-        return b;
-      }
+    public void printBids() {
+        for (int i = bids.size() - 1; i >= 0; i--) {
+            System.out.println(bids.get(i));
+        }
     }
-    return null;
-  }
 
-  public void removeBidFromGivenUser(Owner o) {
-    for(Bid b : bids){
-      if (b.getBidder() == o) {
-        removeBid(b);
-      }
+    public void printThreeBids() {
+        int counter = bids.size();
+        if (bids.size() > 2) {
+            counter = 3;
+        }
+        for (int i = bids.size() - 1; i >= bids.size() - counter; i--) {
+            System.out.println(bids.get(i));
+        }
     }
-  }
 
-  public Dog getDog(){
-    return dog;
-  }
+    public Bid getHighestBid() {
+        if (bids.size() > 0) {
+            return bids.get(bids.size() - 1);
+        }
+        return null;
+    }
 
-  public int getAuctionId() {
-    return auctionId;
-  }
+    public Bid getBidFromGivenUser(Owner o) {
+        for (Bid b : bids) {
+            if (b.getBidder() == o) {
+                return b;
+            }
+        }
+        return null;
+    }
 
-  @Override
-    public String toString(){
-    return String.format("%s is being sold on auction number %s", dog, auctionId);
-  }
+    public void removeBidFromGivenUser(Owner o) {
+        for (Bid b : bids) {
+            if (b.getBidder() == o) {
+                removeBid(b);
+            }
+        }
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public int getAuctionId() {
+        return auctionId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is being sold on auction number %s", dog, auctionId);
+    }
 
 }
--- Owner.java
+++ Owner.java
@@ -1,100 +1,100 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * @author Jonas Andersson Fagerlund joan1043
  */
 public class Owner {
 
-  private String name;
-  private Dog[] ownedDogs = new Dog[0];
+    private String name;
+    private Dog[] ownedDogs = new Dog[0];
 
-
-  public Owner(String name){
-    this.name = name;
-  }
-
-  public String getName(){
-    return name;
-  }
-
-  public boolean ownSpecificDog(Dog searchedDog){
-    for(Dog d: ownedDogs){
-      if (d == searchedDog){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public void addDogToOwner(Dog d){
-    // Söker igenom ägarens lista av hundar för att se om den redan äger hunden.
-    // Behövs eftersom man kan etablera en relation mellan hund och ägare både
-    // från hund och ägarklassen. Utan detta och liknande som finns i dog.java
-    // skulle uppförandet av relation fastna i en evig loop.
-    if (!(Arrays.asList(ownedDogs).contains(d))){
-    
-      Dog[] longerArray = new Dog[ownedDogs.length + 1];
-
-      for(int i = 0; i < ownedDogs.length; i++){
-        longerArray[i] = ownedDogs[i];
-      }
-
-      longerArray[longerArray.length - 1] = d;
-
-      ownedDogs = longerArray;
-
-      d.addOwnerToDog(this);
-    }
-  }
-
-  public void removeAllDogsFromOwner(){
-    for (Dog dogToRemove : ownedDogs) {
-      removeDogFromOwner(dogToRemove);
-    }
-  }
-
-  public void removeDogFromOwner(Dog d){
-    ArrayList<Dog> tempHolding = new ArrayList<>();
-    Dog[] shorterArray = new Dog[ownedDogs.length - 1];
-
-    for(Dog dogToCopy : ownedDogs){
-      if(dogToCopy != d){
-        tempHolding.add(dogToCopy);
-      } else {
-        d.removeOwnerFromOwner();
-      }
+    public Owner(String name) {
+        this.name = name;
     }
 
-    for(int i = 0; i < tempHolding.size(); i++){
-      shorterArray[i] = tempHolding.get(i);
+    public String getName() {
+        return name;
     }
 
-    ownedDogs = shorterArray;
-  }
-
-  public String[] getNameOfOwnedDogs(){
-    String[] nameOfDogs = new String[ownedDogs.length];
-    for(int i = 0; i < ownedDogs.length; i++){
-      nameOfDogs[i] = ownedDogs[i].getName();
+    public boolean ownSpecificDog(Dog searchedDog) {
+        for (Dog d : ownedDogs) {
+            if (d == searchedDog) {
+                return true;
+            }
+        }
+        return false;
     }
-    return nameOfDogs;
-  }
 
-  public boolean haveDogs(){
-    return ownedDogs.length > 0;
-  }
+    public void addDogToOwner(Dog d) {
+        // Söker igenom ägarens lista av hundar för att se om den redan äger hunden.
+        // Behövs eftersom man kan etablera en relation mellan hund och ägare både
+        // från hund och ägarklassen. Utan detta och liknande som finns i dog.java
+        // skulle uppförandet av relation fastna i en evig loop.
+        if (!(Arrays.asList(ownedDogs).contains(d))) {
 
-  @Override
-  public String toString(){
-    return String.format("<%s owns%s>", name, Arrays.toString(nameOfOwnedDogs(ownedDogs)));
-  }
+            Dog[] longerArray = new Dog[ownedDogs.length + 1];
 
-  private String[] nameOfOwnedDogs(Dog[] ownedDogs){
-    String[] nameOfDogs = new String[ownedDogs.length];
-    for(int i = 0; i < ownedDogs.length; i++){
-      nameOfDogs[i] = ownedDogs[i].getName();
+            for (int i = 0; i < ownedDogs.length; i++) {
+                longerArray[i] = ownedDogs[i];
+            }
+
+            longerArray[longerArray.length - 1] = d;
+
+            ownedDogs = longerArray;
+
+            d.addOwnerToDog(this);
+        }
     }
-    return nameOfDogs;
-  }
-  
+
+    public void removeAllDogsFromOwner() {
+        for (Dog dogToRemove : ownedDogs) {
+            removeDogFromOwner(dogToRemove);
+        }
+    }
+
+    public void removeDogFromOwner(Dog d) {
+        ArrayList<Dog> tempHolding = new ArrayList<>();
+        Dog[] shorterArray = new Dog[ownedDogs.length - 1];
+
+        for (Dog dogToCopy : ownedDogs) {
+            if (dogToCopy != d) {
+                tempHolding.add(dogToCopy);
+            } else {
+                d.removeOwnerFromOwner();
+            }
+        }
+
+        for (int i = 0; i < tempHolding.size(); i++) {
+            shorterArray[i] = tempHolding.get(i);
+        }
+
+        ownedDogs = shorterArray;
+    }
+
+    public String[] getNameOfOwnedDogs() {
+        String[] nameOfDogs = new String[ownedDogs.length];
+        for (int i = 0; i < ownedDogs.length; i++) {
+            nameOfDogs[i] = ownedDogs[i].getName();
+        }
+        return nameOfDogs;
+    }
+
+    public boolean haveDogs() {
+        return ownedDogs.length > 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<%s owns%s>", name, Arrays.toString(nameOfOwnedDogs(ownedDogs)));
+    }
+
+    private String[] nameOfOwnedDogs(Dog[] ownedDogs) {
+        String[] nameOfDogs = new String[ownedDogs.length];
+        for (int i = 0; i < ownedDogs.length; i++) {
+            nameOfDogs[i] = ownedDogs[i].getName();
+        }
+        return nameOfDogs;
+    }
+
 }
