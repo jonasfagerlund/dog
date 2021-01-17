52ed45e0ed4ba51b19c8fe711fd52c2c5c317fba
jonasafagerlund <jonas@kladkod.com>
2021-01-17 15:48:36 +0100

    created javaDog, pyDog and kotlinDog

---
+++ kotlinDog/KeyboardInput (kopia).java
@@ -0,0 +1,58 @@
+/**
+ * @author Jonas Andersson Fagerlund joan1043
+ */
+
+import java.util.Scanner;
+
+public class KeyboardInput {
+
+    private static Scanner keyboardInput = new Scanner(System.in);
+
+    public int readInt(String expectedInput) {
+        System.out.print(expectedInput + "?> ");
+        int userInput = keyboardInput.nextInt();
+        keyboardInput.nextLine();
+        return userInput;
+    }
+
+    public double readDouble(String expectedInput) {
+        System.out.print(expectedInput + "?> ");
+        double userInput = keyboardInput.nextDouble();
+        keyboardInput.nextLine();
+        return userInput;
+    }
+
+    public String readString(String expectedInput) {
+        String userInput;
+        do {
+            System.out.print(expectedInput + "?> ");
+            userInput = keyboardInput.nextLine();
+            if (userInput.trim().equals("") || userInput == null) {
+                System.out.println("Error: the name can't be blank");
+            }
+        } while (userInput.trim().equals("") || userInput == null);
+        return capitalize(userInput);
+    }
+
+    public void readNextLine() {
+        keyboardInput.nextLine();
+    }
+
+    public String readEnum(String expectedInput) {
+        System.out.print(expectedInput + "?> ");
+        String userInput = keyboardInput.nextLine();
+        return enumFormat(userInput);
+    }
+
+    private String enumFormat(String s) {
+        s = s.trim();
+        s = s.toUpperCase();
+        s = s.replaceAll("\\s+", "_");
+        return s;
+    }
+
+    private String capitalize(String str) {
+        str = str.trim();
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+}
---
+++ kotlinDog/Owner (kopia).java
@@ -0,0 +1,93 @@
+/**
+ * @author Jonas Andersson Fagerlund joan1043
+ */
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Owner {
+
+    private String name;
+    private Dog[] ownedDogs = new Dog[0];
+
+    public Owner(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean ownSpecificDog(Dog searchedDog) {
+        for (Dog d : ownedDogs) {
+            if (d == searchedDog) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addDogToOwner(Dog d) {
+        // Söker igenom ägarens lista av hundar för att se om den redan äger hunden.
+        // Behövs eftersom man kan etablera en relation mellan hund och ägare både
+        // från hund och ägarklassen. Utan detta och liknande som finns i dog.java
+        // skulle uppförandet av relation fastna i en evig loop.
+        if (!(Arrays.asList(ownedDogs).contains(d))) {
+
+            Dog[] longerArray = new Dog[ownedDogs.length + 1];
+
+            for (int i = 0; i < ownedDogs.length; i++) {
+                longerArray[i] = ownedDogs[i];
+            }
+
+            longerArray[longerArray.length - 1] = d;
+
+            ownedDogs = longerArray;
+
+            d.addOwnerToDog(this);
+        }
+    }
+
+    public void removeAllDogsFromOwner() {
+        for (Dog dogToRemove : ownedDogs) {
+            removeDogFromOwner(dogToRemove);
+        }
+    }
+
+    public void removeDogFromOwner(Dog d) {
+        ArrayList<Dog> tempHolding = new ArrayList<>();
+        Dog[] shorterArray = new Dog[ownedDogs.length - 1];
+
+        for (Dog dogToCopy : ownedDogs) {
+            if (dogToCopy != d) {
+                tempHolding.add(dogToCopy);
+            } else {
+                d.removeOwnerFromOwner();
+            }
+        }
+
+        for (int i = 0; i < tempHolding.size(); i++) {
+            shorterArray[i] = tempHolding.get(i);
+        }
+
+        ownedDogs = shorterArray;
+    }
+
+    public boolean haveDogs() {
+        return ownedDogs.length > 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<%s owns%s>", name, Arrays.toString(nameOfOwnedDogs(ownedDogs)));
+    }
+
+    private String[] nameOfOwnedDogs(Dog[] ownedDogs) {
+        String[] nameOfDogs = new String[ownedDogs.length];
+        for (int i = 0; i < ownedDogs.length; i++) {
+            nameOfDogs[i] = ownedDogs[i].getName();
+        }
+        return nameOfDogs;
+    }
+
+}
